fa42cbe2496fef1a9e91911cb4226e651ac66ef6
uknit23890 <uknit238@gmail.com>
2018-10-28 10:42:15 +0530

    'code'

--- nodeJsFlightListApis/routes/routes.js
+++ nodeJsFlightListApis/routes/routes.js
@@ -1,23 +1,30 @@
 var cors = require('cors');
 var fs = require('fs');
+var couchbase = require('couchbase');
 var flightObj;
+var cluster = new couchbase.Cluster("couchbase://localhost");
+var bucket = cluster.openBucket("flights");
+var FlightsModel = require("./models").FlightsModel;
 const morgan = require('morgan');
 
 fs.readFile('data.json', 'utf8', function(err, data) {
     if (err)
         throw err;
     flightObj = JSON.parse(data);
-});
+ });
+
 var appRouter = function (app) {
 	app.use(cors());//use this after the variable declaration
-
+	
   app.get("/", function(req, res) {
-    res.status(200).send("Welcome to our restful API");
+	res.send('Welcome');
   });
-  
+
+
  app.get('/cities', function(req, res) {
     res.json(flightObj.cities); // return all cities in JSON format
 });
+
 
  app.get('/flights', function(req, res) {
     res.json(flightObj.flights); // return all cities in JSON format
@@ -28,22 +35,22 @@ var appRouter = function (app) {
 	 var f = req.params.from;
 	 var t = req.params.to;
 	 var d = req.params.date;
-	 var flightFound = flightObj.flights.filter(function(item) {
-				if((item.from_city == f && item.to_city == t) && (item.date == d)) {
-					return item;
-				};
-		});
-	res.status(200).send(flightFound); 	
+
+FlightsModel.getAll(f,t,d,function(error, result) {
+	if(error) {
+		return res.status(400).send(error);
+	}
+	res.send(result);
+})	 
  });
  
  app.get("/getFlightsByFlightNumber/:flightNum", function(req, res) {
-	 var f = req.params.flightNum;
-	 var flightFound = flightObj.flights.filter(function(item) {
-				if(item._id == f) {
-					return item;
-				};
-		});
-	res.status(200).send(flightFound); 	
+ bucket.get(req.params.flightNum, function(error, result) {
+	if(error) {
+		return res.status(400).send(error);
+	}
+	res.send(result);
+});
  });
  
 }
